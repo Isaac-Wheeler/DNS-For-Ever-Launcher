e92b858c21d32cc712e81c308658fc29d1c51f5d
Isaac Wheeler <isaacwheeler0@gmail.com>
2013-04-10 02:40:39 -0400

    picture links to main page

--- DNS For Ever luancher/DNS For Ever luancher/Form1.vb
+++ DNS For Ever luancher/DNS For Ever luancher/Form1.vb
@@ -1,3 +1,12 @@
 ﻿Public Class Form1
 
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+
+
+    Private Sub PictureBox1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox1.Click
+        System.Diagnostics.Process.Start("http://dns.mage-tech.org/")
+    End Sub
 End Class
